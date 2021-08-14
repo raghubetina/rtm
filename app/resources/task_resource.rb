8ca6c75e84553447d61c66538418d3ae1940d78c
Raghu Betina <raghu.betina@chicagobooth.edu>
2021-08-14 11:25:41 +0000

    Direct association between UserResource and TaskResource

--- app/resources/task_resource.rb
+++ app/resources/task_resource.rb
@@ -7,6 +7,8 @@ class TaskResource < ApplicationResource
 
   # Direct associations
 
+  belongs_to :user
+
   # Indirect associations
 
 end
--- app/resources/user_resource.rb
+++ app/resources/user_resource.rb
@@ -7,6 +7,8 @@ class UserResource < ApplicationResource
 
   # Direct associations
 
+  has_many   :tasks
+
   # Indirect associations
 
 end
