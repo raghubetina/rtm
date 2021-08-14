e8af5f68e126f1cecaf3d217f3d57a45b8de3092
Raghu Betina <raghu.betina@chicagobooth.edu>
2021-08-14 11:25:24 +0000

    Only task user can modify task

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,4 +1,6 @@
 class TasksController < ApplicationController
+  before_action :current_user_must_be_task_user, only: [:edit, :update, :destroy] 
+
   before_action :set_task, only: [:show, :edit, :update, :destroy]
 
   # GET /tasks
@@ -57,6 +59,14 @@ class TasksController < ApplicationController
 
 
   private
+
+  def current_user_must_be_task_user
+    set_task
+    unless current_user == @task.user
+      redirect_back fallback_location: root_path, alert: "You are not authorized for that."
+    end
+  end
+
     # Use callbacks to share common setup or constraints between actions.
     def set_task
       @task = Task.find(params[:id])
