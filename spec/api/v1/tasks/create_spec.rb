364333d08a2b6618eb7ea699a30a36895af951ba
Raghu Betina <raghu.betina@chicagobooth.edu>
2021-08-14 11:25:41 +0000

    Generate graphiti resources for tasks

---
+++ spec/api/v1/tasks/create_spec.rb
@@ -0,0 +1,31 @@
+require 'rails_helper'
+
+RSpec.describe "tasks#create", type: :request do
+  subject(:make_request) do
+    jsonapi_post "/api/v1/tasks", payload
+  end
+
+  describe 'basic create' do
+    let(:params) do
+      {
+        # ... your attrs here
+      }
+    end
+    let(:payload) do
+      {
+        data: {
+          type: 'tasks',
+          attributes: params
+        }
+      }
+    end
+
+    it 'works' do
+      expect(TaskResource).to receive(:build).and_call_original
+      expect {
+        make_request
+        expect(response.status).to eq(201), response.body
+      }.to change { Task.count }.by(1)
+    end
+  end
+end
